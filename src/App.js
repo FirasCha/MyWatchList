85aefbbde71199833c00fc0d968f82dcb2504f75
FirasCha <cri.firas@gmail.com>
2023-02-22 21:08:15 +0100

    add one picture, css and the js files of tvshow card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import MenuOfWatchList from './components/MenuOfWatchList';
 
 function App() {
   return (
     <div>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<MenuOfWatchList/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/CardTvShowData.js
@@ -0,0 +1,9 @@
+import manifest from "../assets/manifest.jpg"
+const CardTvShowData = [
+    {
+        img : manifest,
+        title : "Manifest",
+        progress : "", 
+    },
+];
+export default CardTvShowData;
