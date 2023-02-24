208c59ea05ded418038d9fbc1e7712ff1aaa10ad
FirasCha <cri.firas@gmail.com>
2023-02-25 00:52:07 +0100

    add navbar menu to tvshow and movie components

--- src/components/CardMovie.js
+++ src/components/CardMovie.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import dataMovie from './CardMovieData'
+import NavBarMenu from './NavBarMenu';
 
 const CardMovie = () => {
     const [ noOfElement, setnoOfElement ] = useState(7);
@@ -9,29 +10,32 @@ const CardMovie = () => {
     }
     const slice = dataMovie.slice(0,noOfElement);
   return (
-    <section className='capy-4 container'>
-        <div style={{textAlign:'center'}} className='row justify-content-center'>
-        {slice?.map((item,index)=>{
-                return(
-                    <div className='col-11 col-md-6 col-lg-3 mx-0 mb-5' key={index}>
-                        <div className='card p-0 overflow-hidden h-100 shadow'>
-                            <img src={item.img} className='card-img-top'/>
-                            <div className='card-body'>
-                                <h5 className='card-title'>{item.title}</h5>
-                                <p className='card-text'>Seasons : {item.max_season}</p>
+    <div>
+        <NavBarMenu/>
+        <section className='capy-4 container'>
+            <div style={{textAlign:'center'}} className='row justify-content-center'>
+            {slice?.map((item,index)=>{
+                    return(
+                        <div className='col-11 col-md-6 col-lg-3 mx-0 mb-5' key={index}>
+                            <div className='card p-0 overflow-hidden h-100 shadow'>
+                                <img src={item.img} className='card-img-top'/>
+                                <div className='card-body'>
+                                    <h5 className='card-title'>{item.title}</h5>
+                                    <p className='card-text'>Seasons : {item.max_season}</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                )
-            })}
-        </div>
-        <button
-            className='btn btn-dark d-block w-100'
-            onClick={() => loadMore()}
-        >
-            Load More
-        </button>
-    </section>
+                    )
+                })}
+            </div>
+            <button
+                className='btn btn-dark d-block w-100'
+                onClick={() => loadMore()}
+            >
+                Load More
+            </button>
+        </section>
+    </div>
   )
 }
 
--- src/components/CardTvShow.js
+++ src/components/CardTvShow.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import dataTvShow from './CardTvShowData'
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import NavBarMenu from './NavBarMenu';
 
 const CardTvShow = () => {
     const [ noOfElement, setnoOfElement ] = useState(7);
@@ -17,49 +18,52 @@ const CardTvShow = () => {
         setShow(true);
     }
   return (
-    <section className='capy-4 container'>
-    <div style={{textAlign:'center'}} className='row justify-content-center'>
-        {slice.map((item,index)=>{
-            return(
-                <div className='col-11 col-md-6 col-lg-3 mx-0 mb-5' key={index}>
-                    <div className='card p-0 overflow-hidden h-100 shadow'>
-                        <img src={item.img} className='card-img-top'/>
-                        <div className='card-body'>
-                            <h5 className='card-title'>{item.title}</h5>
-                            <p className='card-text'>My progress : {item.progress}</p>
-                            <p className='card-text'>Seasons : {item.max_season}</p>
-                            <p className='card-text'>New seasons : {item.new_season}</p>
-                            <Button variant="outline-dark" onClick={handleShow}>
-                                More Details
-                            </Button>
-                            <Modal
-                                show={show}
-                                onHide={handleClose}
-                                backdrop="static"
-                                keyboard={true}
-                                centered={true}
-                                >
-                                <Modal.Header closeButton>
-                                    <Modal.Title>{item.title}</Modal.Title>
-                                </Modal.Header>
-                                <Modal.Body>
-                                    {item.title}
-                                </Modal.Body>
-                            </Modal>
+    <div>
+        <NavBarMenu/>
+        <section className='capy-4 container'>
+            <div style={{textAlign:'center'}} className='row justify-content-center'>
+                {slice.map((item,index)=>{
+                    return(
+                        <div className='col-11 col-md-6 col-lg-3 mx-0 mb-5' key={index}>
+                            <div className='card p-0 overflow-hidden h-100 shadow'>
+                                <img src={item.img} className='card-img-top'/>
+                                <div className='card-body'>
+                                    <h5 className='card-title'>{item.title}</h5>
+                                    <p className='card-text'>My progress : {item.progress}</p>
+                                    <p className='card-text'>Seasons : {item.max_season}</p>
+                                    <p className='card-text'>New seasons : {item.new_season}</p>
+                                    <Button variant="outline-dark" onClick={handleShow}>
+                                        More Details
+                                    </Button>
+                                    <Modal
+                                        show={show}
+                                        onHide={handleClose}
+                                        backdrop="static"
+                                        keyboard={true}
+                                        centered={true}
+                                        >
+                                        <Modal.Header closeButton>
+                                            <Modal.Title>{item.title}</Modal.Title>
+                                        </Modal.Header>
+                                        <Modal.Body>
+                                            {item.title}
+                                        </Modal.Body>
+                                    </Modal>
+                                </div>
+                            </div>
+                            
                         </div>
-                    </div>
-                    
-                </div>
-            )
-        })}
+                    )
+                })}
+            </div>
+                <button
+                    className='btn btn-dark d-block w-100'
+                    onClick={() => loadMore()}
+                >
+                    Load More
+                </button>
+        </section>
     </div>
-    <button
-        className='btn btn-dark d-block w-100'
-        onClick={() => loadMore()}
-    >
-        Load More
-    </button>
-</section>
   )
 }
 
