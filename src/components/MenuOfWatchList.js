d34474c2905e0f173019e35ea105cbe3fcaaf3f5
FirasCha <cri.firas@gmail.com>
2023-02-24 22:29:42 +0100

    add bootstrap-next

--- src/components/MenuOfWatchList.js
+++ src/components/MenuOfWatchList.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import CardTvShowData from './CardTvShowData'
 import CardTvShow from './CardTvShow'
+import CardMovie from './CardMovie'
 
 const MenuOfWatchList = () => {
   return (
     <div className='home-cards'>
         <CardTvShow details={CardTvShowData}/>
+        <CardMovie/>
     </div>
   )
 }
