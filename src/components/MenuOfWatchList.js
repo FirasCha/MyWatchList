ab073f55c14c9c5a1115b744de776646fab8d4d4
FirasCha <cri.firas@gmail.com>
2023-02-25 00:46:32 +0100

    add navbar

--- src/components/MenuOfWatchList.js
+++ src/components/MenuOfWatchList.js
@@ -1,21 +1,43 @@
 import React from 'react'
-import CardTvShow from './CardTvShow'
-import CardMovie from './CardMovie'
+import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
-import { Link } from 'react-router-dom'
+import Navbar from 'react-bootstrap/Navbar';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
 const MenuOfWatchList = () => {
   return (
-    // <Nav defaultActiveKey="/" className="flex-column">
-    //   <Nav.Link to="/">Active</Nav.Link>
-    //   <Nav.Link eventKey="link-1">Link</Nav.Link>
-    //   <Nav.Link eventKey="link-2">Link</Nav.Link>
-    // </Nav>
-    <div>
-      <Link to="/" > Menu</Link>
-      <Link to="/tvshow" > TV Shows</Link>
-      <Link to="/mymovies" > Movies</Link>
-    </div>
+    // <div>
+    //   <Link to="/" > Menu</Link>
+    //   <Link to="/tvshow" > TV Shows</Link>
+    //   <Link to="/mymovies" > Movies</Link>
+    // </div>
+    <>
+      {[false].map((expand) => (
+        <Navbar key={expand} bg="light" expand={expand} className="mb-3">
+          <Container fluid>
+            <Navbar.Brand href="/">Menu</Navbar.Brand>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                  Menu
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                  <Nav.Link href="/tvshow" >TV Shows</Nav.Link>
+                  <Nav.Link href="/mymovies">Movies</Nav.Link>
+                </Nav>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
+    </>
                         
   )
 }
