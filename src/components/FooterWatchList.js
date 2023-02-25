afad21cb311c0af7f7cfe5f48ecd54866f81b271
FirasCha <cri.firas@gmail.com>
2023-02-25 08:14:28 +0100

    finishing footer

--- src/components/FooterWatchList.js
+++ src/components/FooterWatchList.js
@@ -1,44 +1,96 @@
 import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
 
 const FooterWatchList = () => {
     return (
-        <footer className="page-footer font-small blue pt-4" style={{ backgroundColor: '#caced1' }}>
-            <div className="container-fluid text-center text-md-left">
-                <div className="row">
-                    <div className="col-md-6 mt-md-0 mt-3">
-                        <h5 className="text-uppercase">Footer Content</h5>
-                        <p>Here you can use rows and columns to organize your footer content.</p>
-                    </div>
-
-                    <hr className="clearfix w-100 d-md-none pb-0"/>
-
-                    <div className="col-md-3 mb-md-0 mb-3">
-                        <h5 className="text-uppercase">Links</h5>
-                        <ul className="list-unstyled">
-                            <li><a href="#!">Link 1</a></li>
-                            <li><a href="#!">Link 2</a></li>
-                            <li><a href="#!">Link 3</a></li>
-                            <li><a href="#!">Link 4</a></li>
-                        </ul>
-                    </div>
-
-                    <div className="col-md-3 mb-md-0 mb-3">
-                        <h5 className="text-uppercase">Links</h5>
-                        <ul className="list-unstyled">
-                            <li><a href="#!">Link 1</a></li>
-                            <li><a href="#!">Link 2</a></li>
-                            <li><a href="#!">Link 3</a></li>
-                            <li><a href="#!">Link 4</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-
+        <footer className='text-center text-white' style={{ backgroundColor: '#caced1' }}>
+            <Container className='p-3'>
+                <section className=''>
+                    <Row>
+                        <Col lg='2' md='12' className='mb-4 mb-md-0'>
+                            <div color='light' className='bg-image hover-overlay shadow-1-strong rounded'>
+                            <a href='/'>
+                            <div
+                                
+                                className='mask'
+                                style={{ backgroundColor: 'rgba(251, 251, 251, 0.2)' }}
+                            >
+                                <img src='https://cdn2.hubspot.net/hubfs/53/homepage-web-design.jpg' className='w-100' />
+                            </div>
+                            </a>
+                            </div>
+                        </Col>
+                        <Col lg='2' md='12' className='mb-4 mb-md-0'>
+                            <div color='light' className='bg-image hover-overlay shadow-1-strong rounded'>
+                            <a href='/tvshow'>
+                            <div
+                                className='mask'
+                                style={{ backgroundColor: 'rgba(251, 251, 251, 0.2)' }}
+                            >
+                                <img src='https://ggsc.s3.amazonaws.com/images/uploads/Greater_Goodies_TV_2022.jpeg' className='w-100' />
+                            </div>
+                            </a>
+                            </div>
+                        </Col>
+                        <Col lg='2' md='12' className='mb-4 mb-md-0'>
+                            <div color='light' className='bg-image hover-overlay shadow-1-strong rounded'>
+                            
+                            <a href='/mymovies'>
+                            <div
+                                className='mask'
+                                style={{ backgroundColor: 'rgba(251, 251, 251, 0.2)' }}
+                            >
+                                <img src='https://telegraphstar.com/wp-content/uploads/2020/04/9x.jpg' className='w-100' />
+                            </div>
+                            </a>
+                            </div>
+                        </Col>
+                        <Col lg='2' md='12' className='mb-4 mb-md-0'>
+                            <div color='light' className='bg-image hover-overlay shadow-1-strong rounded'>
+                            
+                            <a href='/myanime'>
+                            <div
+                                className='mask'
+                                style={{ backgroundColor: 'rgba(251, 251, 251, 0.2)' }}
+                            >
+                                <img src='https://unbumf.com/wp-content/uploads/2018/09/thumb.jpg' className='w-100' />
+                            </div>
+                            </a>
+                            </div>
+                        </Col>
+                        <Col lg='2' md='12' className='mb-4 mb-md-0'>
+                            <div color='light' className='bg-image hover-overlay shadow-1-strong rounded'>
+                            
+                            <a href='#!'>
+                            <div
+                                className='mask'
+                                style={{ backgroundColor: 'rgba(251, 251, 251, 0.2)' }}
+                            >
+                                <img src='https://geekflare.com/wp-content/uploads/2022/10/Manga.webp' className='w-100' />
+                            </div>
+                            </a>
+                            </div>
+                        </Col>
+                        {/* <Col lg='2' md='12' className='mb-4 mb-md-0'>
+                            <div color='light' className='bg-image hover-overlay shadow-1-strong rounded'>
+                            <img src='https://mdbcdn.b-cdn.net/img/new/fluid/city/116.webp' className='w-100' />
+                            <a href='#!'>
+                            <div
+                                className='mask'
+                                style={{ backgroundColor: 'rgba(251, 251, 251, 0.2)' }}
+                            ></div>
+                            </a>
+                            </div>
+                        </Col> */}
+                        
+                    </Row>
+                </section>
+            </Container>
             <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-                © 2020 Copyright:
-                <a className='text-white' href='https://mdbootstrap.com/'>
-                    MDBootstrap.com
-                </a>
+                © 2020 Copyright Firas Chaouch
+                {/* <a className='text-white'>
+                    Firas Chaouch
+                </a> */}
             </div>
         </footer>
     )
--- src/components/NavBarMenu.js
+++ src/components/NavBarMenu.js
@@ -8,7 +8,7 @@ const NavBarMenu = () => {
   return (
     <>
       {[false].map((expand) => (
-        <Navbar key={expand} bg="light" expand={expand} className="mb-3">
+        <Navbar key={expand} expand={expand} className="mb-3" style={{ backgroundColor: '#caced1' }}>
           <Container fluid>
             <Navbar.Brand href="/">Menu</Navbar.Brand>
             <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
@@ -17,12 +17,12 @@ const NavBarMenu = () => {
               aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
               placement="end"
             >
-              <Offcanvas.Header closeButton>
+              <Offcanvas.Header closeButton style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
                 <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
                   Menu
                 </Offcanvas.Title>
               </Offcanvas.Header>
-              <Offcanvas.Body>
+              <Offcanvas.Body style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
                 <Nav className="justify-content-end flex-grow-1 pe-3">
                   <Nav.Link href="/" >Menu</Nav.Link>
                   <Nav.Link href="/tvshow" >TV Shows</Nav.Link>
