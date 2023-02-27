472807e3e1ebcc3723493ee7708e055cb13653f6
FirasCha <cri.firas@gmail.com>
2023-02-27 20:04:21 +0100

    add manga data json file

---
+++ src/components/CardMangaData.js
@@ -0,0 +1,10 @@
+const CardMangaData = [
+    {
+        img : Through_My_Window,
+        title : "Trough my window",
+        Released_Year : "2022",
+        duration : "1h 56m",
+     },
+
+];
+export default CardMangaData;
