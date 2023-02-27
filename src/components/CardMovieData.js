012016a31d973f7de5d7b92483abdef16fa5b537
FirasCha <cri.firas@gmail.com>
2023-02-27 19:51:25 +0100

    still adding more pictures

--- src/components/CardMovieData.js
+++ src/components/CardMovieData.js
@@ -14,6 +14,19 @@ import The_Transporter_2002 from "../assets/The-Transporter-2002.jpg"
 import red from "../assets/red.jpg"
 import taken_2 from "../assets/taken2.jpg"
 import The_Italian_Job from "../assets/The_Italian_Job.jpg"
+import Olympus_Has_Fallen from "../assets/Olympus_Has_Fallen.jpg"
+import the_world_war_z from "../assets/the_world_war_z.jpg"
+import training_day from "../assets/training_day.jpg"
+import Wonder_Woman from "../assets/Wonder_Woman.jpg"
+import Kung_Fu_Panda from "../assets/Kung_Fu_Panda.jpg"
+import shrek from "../assets/shrek.jpg"
+import king_kong from "../assets/king_kong_2.jpg"
+import Despicable_Me from "../assets/Despicable_Me.jpg"
+import Madagascar from "../assets/Madagascar.jpg"
+import Madagascar_2 from "../assets/Madagascar2.jpg"
+import How_to_Train_Your_Dragon2 from "../assets/How_to_Train_Your_Dragon2.jpg"
+import shrek_2 from "../assets/shrek2.webp"
+import How_to_Train_Your_Dragon from "../assets/How_to_Train_Your_Dragon.webp"
 
 const CardMovieData = [
     {
@@ -113,79 +126,79 @@ const CardMovieData = [
         duration : "1h 51m",
      },
      {
-        img : the_A_team,
+        img : Olympus_Has_Fallen,
         title : "Olympus Has Fallen",
         Released_Year : "2013",
         duration : "1h 59m",
      },
      {
-        img : the_A_team,
+        img : the_world_war_z,
         title : "World War Z",
         Released_Year : "2013",
         duration : "1h 56m",
      },
      {
-        img : the_A_team,
+        img : training_day,
         title : "Training Day",
         Released_Year : "2001",
         duration : "2h 2m",
      },
      {
-        img : the_A_team,
+        img : Wonder_Woman,
         title : "Wonder Woman",
         Released_Year : "2017",
         duration : "2h 21m",
      },
      {
-        img : the_A_team,
+        img : Kung_Fu_Panda,
         title : "Kung Fu Panda",
         Released_Year : "2008",
         duration : "1h 32m",
      },
      {
-        img : the_A_team,
+        img : shrek,
         title : "Shrek",
         Released_Year : "2001",
         duration : "1h 30m",
      },
      {
-        img : the_A_team,
+        img : shrek_2,
         title : "Shrek 2",
         Released_Year : "2004",
         duration : "1h 33m",
      },
      {
-        img : the_A_team,
+        img : king_kong,
         title : "King Kong",
         Released_Year : "2005",
         duration : "3h 7m",
      },
      {
-        img : the_A_team,
+        img : Despicable_Me,
         title : "Despicable Me",
         Released_Year : "2010",
         duration : "1h 35m",
      },
      {
-        img : the_A_team,
+        img : Madagascar,
         title : "Madagascar",
         Released_Year : "2005",
         duration : "1h 26m",
      },
      {
-        img : the_A_team,
+        img : Madagascar_2,
         title : "Madagascar: Escape 2 Africa",
         Released_Year : "2008",
         duration : "1h 29m",
      },
      {
-        img : the_A_team,
+        img : How_to_Train_Your_Dragon,
         title : "How to Train Your Dragon",
         Released_Year : "2010",
         duration : "1h 38m",
      },
      {
-        img : the_A_team,
+        img : How_to_Train_Your_Dragon2,
         title : "How to Train Your Dragon 2",
         Released_Year : "2014",
         duration : "1h 42m",
