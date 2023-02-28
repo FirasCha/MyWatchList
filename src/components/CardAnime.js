0490341c1c0e4a0cd7f22df6c08968e194b80287
FirasCha <cri.firas@gmail.com>
2023-02-28 18:18:26 +0100

    create the interface of anime but no pictures

--- src/components/CardAnime.js
+++ src/components/CardAnime.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React , { useState } from 'react'
+import NavBarMenu from './NavBarMenu';
+import FooterWatchList from './FooterWatchList';
+import dataAnime from './CardAnimeData'
 
 const CardAnime = () => {
+    const [ noOfElement, setnoOfElement ] = useState(7);
+    const loadMore = () => {
+        setnoOfElement(noOfElement+5);
+    }
+    //console.log(dataAnime)
+    const slice = dataAnime.slice(0,noOfElement);
   return (
     <div>
-        
+        <NavBarMenu/>
+        <section className='capy-4 container'>
+            <div style={{textAlign:'center'}} className='row justify-content-center'>
+                {slice.map((item,index)=>{
+                    return(
+                        <div className='col-11 col-md-6 col-lg-3 mx-0 mb-5' key={index}>
+                            <div className='card p-0 overflow-hidden h-100 shadow'>
+                                <img src={item.img} alt="" className='card-img-top'/>
+                                <div className='card-body'>
+                                    <h5 className='card-title'>{item.title}</h5>
+                                    <p className='card-text'>Genres : {item.Genres}</p>
+                                    <p className='card-text'>Studio : {item.Studio}</p>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    )
+                })}
+            </div>
+                <button
+                    className='btn btn-dark d-block w-100'
+                    onClick={() => loadMore()}
+                >
+                    Load More
+                </button>
+                <br/>
+        </section>
+        <FooterWatchList/> 
     </div>
   )
 }
