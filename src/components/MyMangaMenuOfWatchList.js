680b623437476832f85180e163be64674a8e9aab
FirasCha <cri.firas@gmail.com>
2023-03-01 18:55:32 +0100

    add conditions for manga watch list section to not repeat the same picture

--- src/components/MyMangaMenuOfWatchList.js
+++ src/components/MyMangaMenuOfWatchList.js
@@ -4,11 +4,31 @@ import Card from 'react-bootstrap/Card';
 
 const MyMangaMenuOfWatchList = () => {
     const StartIndiceOfManga0 = Math.floor(Math.random()*dataManga.length)
-    const StartIndiceOfManga1 = Math.floor(Math.random()*dataManga.length)
-    const StartIndiceOfManga2 = Math.floor(Math.random()*dataManga.length)
-    const StartIndiceOfManga3 = Math.floor(Math.random()*dataManga.length)
-    const StartIndiceOfManga4 = Math.floor(Math.random()*dataManga.length)
-    const StartIndiceOfManga5 = Math.floor(Math.random()*dataManga.length)
+    var StartIndiceOfManga1 = Math.floor(Math.random()*dataManga.length)
+    var StartIndiceOfManga2 = Math.floor(Math.random()*dataManga.length)
+    var StartIndiceOfManga3 = Math.floor(Math.random()*dataManga.length)
+    var StartIndiceOfManga4 = Math.floor(Math.random()*dataManga.length)
+    var StartIndiceOfManga5 = Math.floor(Math.random()*dataManga.length)
+    if (StartIndiceOfManga1===StartIndiceOfManga0)
+    {
+        StartIndiceOfManga1 = Math.floor(Math.random()*dataManga.length)
+    }
+    if ((StartIndiceOfManga2===StartIndiceOfManga1)||(StartIndiceOfManga2===StartIndiceOfManga0))
+    {
+        StartIndiceOfManga2 = Math.floor(Math.random()*dataManga.length)
+    }
+    if ((StartIndiceOfManga3===StartIndiceOfManga2)||(StartIndiceOfManga3===StartIndiceOfManga1)||(StartIndiceOfManga3===StartIndiceOfManga0))
+    {
+        StartIndiceOfManga3 = Math.floor(Math.random()*dataManga.length)
+    }
+    if ((StartIndiceOfManga4===StartIndiceOfManga3)||(StartIndiceOfManga4===StartIndiceOfManga2)||(StartIndiceOfManga4===StartIndiceOfManga1)||(StartIndiceOfManga4===StartIndiceOfManga0))
+    {
+        StartIndiceOfManga4 = Math.floor(Math.random()*dataManga.length)
+    }
+    if ((StartIndiceOfManga5===StartIndiceOfManga4)||(StartIndiceOfManga5===StartIndiceOfManga3)||(StartIndiceOfManga5===StartIndiceOfManga2)||(StartIndiceOfManga5===StartIndiceOfManga1)||(StartIndiceOfManga5===StartIndiceOfManga0))
+    {
+        StartIndiceOfManga5 = Math.floor(Math.random()*dataManga.length)
+    }
   return (
     <Card className="text-center">
     <Card.Header><h5>My Manga</h5></Card.Header>
