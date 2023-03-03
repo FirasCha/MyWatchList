2a060b9061348697ab1caf745b1427d35f9a61a8
FirasCha <cri.firas@gmail.com>
2023-03-03 20:29:49 +0100

    minimize code of manga menu

--- src/components/MyMangaMenuOfWatchList.js
+++ src/components/MyMangaMenuOfWatchList.js
@@ -3,32 +3,27 @@ import dataManga from './CardMangaData'
 import Card from 'react-bootstrap/Card';
 
 const MyMangaMenuOfWatchList = () => {
-    const StartIndiceOfManga0 = Math.floor(Math.random()*dataManga.length)
-    var StartIndiceOfManga1 = Math.floor(Math.random()*dataManga.length)
-    var StartIndiceOfManga2 = Math.floor(Math.random()*dataManga.length)
-    var StartIndiceOfManga3 = Math.floor(Math.random()*dataManga.length)
-    var StartIndiceOfManga4 = Math.floor(Math.random()*dataManga.length)
-    var StartIndiceOfManga5 = Math.floor(Math.random()*dataManga.length)
-    if (StartIndiceOfManga1===StartIndiceOfManga0)
-    {
-        StartIndiceOfManga1 = Math.floor(Math.random()*dataManga.length)
-    }
-    if ((StartIndiceOfManga2===StartIndiceOfManga1)||(StartIndiceOfManga2===StartIndiceOfManga0))
-    {
-        StartIndiceOfManga2 = Math.floor(Math.random()*dataManga.length)
-    }
-    if ((StartIndiceOfManga3===StartIndiceOfManga2)||(StartIndiceOfManga3===StartIndiceOfManga1)||(StartIndiceOfManga3===StartIndiceOfManga0))
-    {
-        StartIndiceOfManga3 = Math.floor(Math.random()*dataManga.length)
-    }
-    if ((StartIndiceOfManga4===StartIndiceOfManga3)||(StartIndiceOfManga4===StartIndiceOfManga2)||(StartIndiceOfManga4===StartIndiceOfManga1)||(StartIndiceOfManga4===StartIndiceOfManga0))
-    {
-        StartIndiceOfManga4 = Math.floor(Math.random()*dataManga.length)
-    }
-    if ((StartIndiceOfManga5===StartIndiceOfManga4)||(StartIndiceOfManga5===StartIndiceOfManga3)||(StartIndiceOfManga5===StartIndiceOfManga2)||(StartIndiceOfManga5===StartIndiceOfManga1)||(StartIndiceOfManga5===StartIndiceOfManga0))
-    {
-        StartIndiceOfManga5 = Math.floor(Math.random()*dataManga.length)
-    }
+  let StartIndiceOfManga;
+  let compareMangaIndiceList = []; 
+  for(let i = 0; i<=5; i++){
+    StartIndiceOfManga = Math.floor(Math.random()*dataManga.length);
+    compareMangaIndiceList.push(StartIndiceOfManga);
+  }
+  //No duplicate element  
+  for(let i = 0; i<=5; i++) {  
+    for(let j = i+1; j<=5; j++) {  
+        if(compareMangaIndiceList[i] === compareMangaIndiceList[j])  
+            {
+              compareMangaIndiceList[j]=Math.floor(Math.random()*dataManga.length);
+            }  
+        }  
+     }  
+  /**Input the elements */
+  let mangaList = [];
+  for (let i = 0; i<=5; i++){
+    let j=compareMangaIndiceList[i]
+    mangaList.push(dataManga[j].img);
+  }
   return (
     <Card className="text-center">
     <Card.Header><h5>My Manga</h5></Card.Header>
@@ -37,32 +32,32 @@ const MyMangaMenuOfWatchList = () => {
         <div style={{textAlign:'center'}} className='row justify-content-center'>
           <div className='col-11 col-md-6 col-lg-2 mx-0 mb-5'>
             <div className='card p-0 overflow-hidden h-100 shadow'>
-              <img src={dataManga[StartIndiceOfManga0].img} alt="" className='card-img-top'/>
+              <img src={mangaList[0]} alt="" className='card-img-top'/>
             </div> 
           </div>
           <div className='col-11 col-md-6 col-lg-2 mx-0 mb-5'>
             <div className='card p-0 overflow-hidden h-100 shadow'>
-              <img src={dataManga[StartIndiceOfManga1].img} alt="" className='card-img-top'/>
+              <img src={mangaList[1]} alt="" className='card-img-top'/>
             </div> 
           </div>
           <div className='col-11 col-md-6 col-lg-2 mx-0 mb-5'>
             <div className='card p-0 overflow-hidden h-100 shadow'>
-              <img src={dataManga[StartIndiceOfManga2].img} alt="" className='card-img-top'/>
+              <img src={mangaList[2]} alt="" className='card-img-top'/>
             </div> 
           </div>
           <div className='col-11 col-md-6 col-lg-2 mx-0 mb-5'>
             <div className='card p-0 overflow-hidden h-100 shadow'>
-              <img src={dataManga[StartIndiceOfManga3].img} alt="" className='card-img-top'/>
+              <img src={mangaList[3]} alt="" className='card-img-top'/>
             </div> 
           </div>
           <div className='col-11 col-md-6 col-lg-2 mx-0 mb-5'>
             <div className='card p-0 overflow-hidden h-100 shadow'>
-              <img src={dataManga[StartIndiceOfManga4].img} alt="" className='card-img-top'/>
+              <img src={mangaList[4]} alt="" className='card-img-top'/>
             </div> 
           </div>
           <div className='col-11 col-md-6 col-lg-2 mx-0 mb-5'>
             <div className='card p-0 overflow-hidden h-100 shadow'>
-              <img src={dataManga[StartIndiceOfManga5].img} alt="" className='card-img-top'/>
+              <img src={mangaList[5]} alt="" className='card-img-top'/>
             </div> 
           </div>                
         </div>
