dc939eb750e4e9521e270ebbdb8518d4e4dadc01
FirasCha <cri.firas@gmail.com>
2023-03-04 10:39:55 +0100

    sort work perfectly by name and interface look perfect

--- src/components/CardManga.js
+++ src/components/CardManga.js
@@ -9,40 +9,29 @@ const CardManga = () => {
     const loadMore = () => {
         setnoOfElement(noOfElement+5);
     }
-    const slice = dataManga.slice(0,noOfElement);
     let i=0;
     let ListSortNameManga = []
-    let ListSortReverseNameManga = []
     while(i<dataManga.length)
     {
         ListSortNameManga.push(dataManga[i])
-        //ListSortReverseNameManga.push(dataManga[i].title)
         i++
-    }
-    console.log(ListSortNameManga)
-    for(let i=0;i<ListSortNameManga.length;i++)
-    {
-        console.log(ListSortNameManga[i].title)
     }
     const [sortState, setSortState] = useState("none");
     const sortMethods = {
         none: { method: (a, b) => null },
-        ascending: { method: (a, b) => (a.title < b.title ? -1 : 1) },
-        descending: { method: (a, b) => (a.title > b.title ? -1 : 1)},
+        SortA_ZName: { method: (a, b) => (a.title < b.title ? -1 : 1) },
+        SortZ_AName: { method: (a, b) => (a.title > b.title ? -1 : 1)},
       };
-    // console.log("List Sort reverse Manga",sortReverse)
-    // console.log("List Sort Manga",sort)
-    // const data = ["Banana", "Orange", "Apple", "Mango"];
-    // const sort = data.sort()
-    // console.log(sort)
+      
+    let slice = ListSortNameManga.slice(0,noOfElement);
   return (
     <div>
         <NavBarMenu/>
         <section className='capy-4 container'>
             <select defaultValue={'DEFAULT'} onChange={(e) => setSortState(e.target.value)}>
                 <option value="DEFAULT" disabled></option>
-                <option value="ascending">Ascending</option>
-                <option value="descending">Descending</option>
+                <option value="SortA_ZName">Sort A-Z by Name</option>
+                <option value="SortZ_AName">Sort Z-A by Name</option>
             </select>
             <ul>
                 {ListSortNameManga.sort(sortMethods[sortState].method).map((el, i) => (
@@ -50,21 +39,7 @@ const CardManga = () => {
                 ))}
             </ul>
             <div style={{textAlign:'center'}} className='row justify-content-center'>
-                {ListSortNameManga.sort(sortMethods[sortState].method).map((item, index) =>{
-                    return(
-                        <div className='col-11 col-md-6 col-lg-3 mx-0 mb-5' key={index}>
-                            <div className='card p-0 overflow-hidden h-100 shadow'>
-                                <img src={item.img} alt="" className='card-img-top'/>
-                                <div className='card-body'>
-                                    <h5 className='card-title'>{item.title}</h5>
-                                </div>
-                            </div>
-                        </div>
-                    )
-                })}
-            </div>
-            <div style={{textAlign:'center'}} className='row justify-content-center'>
-                {slice.map((item,index)=>{
+                {slice.sort(sortMethods[sortState].method).map((item, index) =>{
                     return(
                         <div className='col-11 col-md-6 col-lg-3 mx-0 mb-5' key={index}>
                             <div className='card p-0 overflow-hidden h-100 shadow'>
@@ -76,7 +51,6 @@ const CardManga = () => {
                                     <p className='card-text'>Genres : {item.Genres}</p>
                                 </div>
                             </div>
-                            
                         </div>
                     )
                 })}
